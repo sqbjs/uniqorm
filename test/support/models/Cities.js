ab7de9669eefb8c17445f5598d4f6397daef4bb0
Eray Hanoğlu <erayszn@gmx.net>
2018-12-22 16:49:46 +0300

    [+] Added suport for defining of join type. [*] Optimized Finder

--- test/support/models/Cities.js
+++ test/support/models/Cities.js
@@ -3,6 +3,7 @@ const merge = require('putil-merge');
 module.exports = merge.deep({}, require('./Cities.json'), {
   fields: {
     country: {
+      joinType: 'inner',
       foreignModel: 'uniqorm_1.Countries'
     },
     countryName: {
